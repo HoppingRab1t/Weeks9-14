3e4969bc10e5eb61ecc2b6e6c796c7bf7b268f65
Daniel Yeung <yeungdan@shernet.sheridancollege.ca>
2026-03-10 11:10:58 -0400

    Did some input stuff

---
+++ Assets/Scripts/Controller Input.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ControllerInput : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed = 5;
+    public Vector2 movement;
+    public AudioSource SFX;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position += (Vector3) movement * speed * Time.deltaTime;
+        transform.position = movement;
+    }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>();
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        Debug.Log("attack"+context.phase);
+        if(context.performed == true)
+        {
+            SFX.Play();
+        }
+    }
+    public void OnPoint(InputAction.CallbackContext context)
+    {
+        movement = Camera.main.ScreenToWorldPoint(context.ReadValue<Vector2>());
+
+    }
+}
