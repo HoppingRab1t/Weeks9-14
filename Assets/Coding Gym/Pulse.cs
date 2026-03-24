937a8d98a6b3cb07a6d8c6c1093fbe3a85bc728f
Daniel Yeung <yeungdan@shernet.sheridancollege.ca>
2026-03-24 10:51:37 -0400

    Added some more sound effects and did some coding gym stuff.

---
+++ Assets/Coding Gym/Pulse.cs
@@ -0,0 +1,35 @@
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
+using UnityEngine;
+
+public class Pulse : MonoBehaviour
+{
+    public Vector2 pos;
+    public AnimationCurve curve;
+    float time = 0f;
+    public int speed = 10;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += speed/10 * Time.deltaTime;
+        if (time > 1)
+        {
+            time = 0;
+        }
+
+        pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        if (pos.x >= 10)
+        {
+            pos.x = -10;
+        }
+        pos.y = curve.Evaluate(time)*5;
+
+        transform.position = pos;
+    }
+}
