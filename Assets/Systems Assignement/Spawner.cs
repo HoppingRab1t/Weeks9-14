c36a5a7eb606b40d0104c56ab9b2728ae4a76f33
CHOCOBO_DY\danie <YeungDan@Sheridancollege.ca>
2026-04-01 09:25:27 -0400

    Attempting to fix some issues with git

--- Assets/Systems Assignement/Spawner.cs
+++ Assets/Systems Assignement/Spawner.cs
@@ -1,21 +1,61 @@
 using NUnit.Framework;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class Spawner : MonoBehaviour
 {
     public GameObject prefab;
-    public GameObject target;'
+    public GameObject target;
+    public Transform parent;
 
+    public float timer;
+
+    Vector2 pos;
+    Vector2 direciton;
     public List<GameObject> object_list;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        target = Instantiate (prefab,transform.position);
+        pos = transform.position;
+
+        //position of the thing
+        pos.y += -1 * Time.deltaTime;
+
+        timer += 1 * Time.deltaTime;
+
+        if (timer > 2)
+        {
+            prefab = Instantiate(target, new Vector2(Random.Range(-10, 10), 4), transform.rotation, parent);
+            object_list.Add(prefab);
+
+            for (int i = 0; i < object_list.Count; i += 1)
+            {
+
+            }
+            //utter peiece of poop
+
+
+            timer = 0;
+        }
+
+        transform.position = pos;
+        transform.up = direciton;
+        //control k and then d
+    }
+    public void OnPoint(InputAction.CallbackContext context)
+    {
+        Vector2 direciton = Camera.main.ScreenToWorldPoint(context.ReadValue<Vector2>());
+        Debug.Log("poo");
     }
 }
